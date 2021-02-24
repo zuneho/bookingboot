51df40cfa6e5e1e3c721db49bdb6b395d18ad9d7
zuneho <zuneho@gmail.com>
2021-02-24 21:49:10 +0900

    init

---
+++ src/main/java/io/github/zuneho/bookingboot/BookingbootApplication.java
@@ -0,0 +1,13 @@
+package io.github.zuneho.bookingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/zuneho/bookingboot/BookingbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zuneho.bookingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
