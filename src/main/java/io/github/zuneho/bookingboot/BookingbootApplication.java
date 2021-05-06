8a05c9198e97636898c31ef0285c90bc7679be31
june.jung <june.jung@yanolja.com>
2021-05-07 02:53:24 +0900

    [ModernJava]day04 -> practice JPA (set up h2 console connect in TCP base)

--- src/main/java/io/github/zuneho/bookingboot/BookingbootApplication.java
+++ src/main/java/io/github/zuneho/bookingboot/BookingbootApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookingbootApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BookingbootApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BookingbootApplication.class, args);
+    }
 }
